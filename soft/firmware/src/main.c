2c98acb23bd668431bc47f5fc93868fe4e5b471e
Owen Trueblood <jmptable@mit.edu>
2013-12-08 00:15:54 -0500

    Decode quadrature encoder output and show direction on two LEDs.

--- soft/firmware/src/main.c
+++ soft/firmware/src/main.c
@@ -9,30 +9,73 @@
 
 // pins
 
-#define PIN_1A		PC0
-#define PIN_2A		PC2
-#define PIN_EN		PC3
+#define PIN_M_1A		PC0
+#define PIN_M_2A		PC1
+#define PIN_M_EN		PC2
 
-#define PIN_LED1	PD6
-#define PIN_LED2	PD7
+#define PIN_LED0		PD6
+#define PIN_LED1		PD7
 
-#define PIN_A		PB0
-#define PIN_B		PB1
+#define PIN_A			PB0
+#define PIN_B			PB1
 
 // constants
 
+volatile uint8_t hist_portb = 0xFF;     // default is high because the pull-up
+
+ISR(PCINT0_vect) {
+	uint8_t changed;
+
+	changed = PINB ^ hist_portb;
+	hist_portb = PINB;
+
+	if(changed & (1 << PB0)) {
+		if(PINB & (1 << PB0)) { // pb0 rising edge
+			if(PINB & (1 << PB1)) {
+				// forward
+				PORTD &= ~(1 << PIN_LED1);
+				PORTD |= 1 << PIN_LED0;
+			} else {
+				// backward
+				PORTD &= ~(1 << PIN_LED0);
+				PORTD |= 1 << PIN_LED1;
+			}
+		}
+	}
+}
+
 int main(void) {
+	// setup pin change interrupts
+	PCICR = 1 << PCIE0;
+	PCMSK0 = 1 << PCINT0;
+
 	// setup pins
-	DDRC |= (1 << PIN_EN) | (1 << PIN_1A) | (1 << PIN_2A);
-	DDRD |= (1 << PIN_LED1) | (1 << PIN_LED2);
+	DDRC |= (1 << PIN_M_EN) | (1 << PIN_M_1A) | (1 << PIN_M_2A);
+	DDRD |= (1 << PIN_LED0) | (1 << PIN_LED1);
+
+	// motor enabled
+	DDRC |= (1 << 2);
+	PORTC |= (1 << 2);
+
+	//PORTD |= (1 << PIN_LED0) | (1 << PIN_LED1);
+
+	sei();
 
 	forever {
-		PORTD &= ~(1 << PIN_LED1);
-		PORTD |= 1 << PIN_LED2;
-		_delay_ms(500);
-		PORTD &= ~(1 << PIN_LED2);
-		PORTD |= 1 << PIN_LED1;
-		_delay_ms(500);
+		// forward
+		PORTC &= ~(1 << PIN_M_2A);
+		PORTC |= 1 << PIN_M_1A;
+		_delay_ms(1000);
+		
+		// backward
+		PORTC &= ~(1 << PIN_M_1A);
+		PORTC |= 1 << PIN_M_2A;
+		_delay_ms(1000);
+
+		// stop
+		PORTC &= ~(1 << PIN_M_1A);
+		PORTC &= ~(1 << PIN_M_2A);
+		_delay_ms(1000);
 	}
 
 	return 0;
