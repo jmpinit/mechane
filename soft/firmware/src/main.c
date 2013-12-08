cf3641a4131d21f0cf1366e12bbddadefb3a2c68
Owen Trueblood <jmptable@mit.edu>
2013-12-08 00:57:05 -0500

    Say hello at 4800 baud over serial.

--- soft/firmware/src/main.c
+++ soft/firmware/src/main.c
@@ -7,6 +7,19 @@
 
 #define forever for(;;)
 
+#define CLK_NONE	1
+#define CLK_8		2
+#define CLK_64		3
+#define CLK_256		4
+#define CLK_1024	5
+
+// constants
+
+#define BAUD 4800
+
+#define LEFT	1
+#define RIGHT	-1
+
 // pins
 
 #define PIN_M_1A		PC0
@@ -19,9 +32,16 @@
 #define PIN_A			PB0
 #define PIN_B			PB1
 
-// constants
+// motor info
+typedef struct {
+	int8_t dir;
+	int inverse_speed;
+} Motor;
 
-volatile uint8_t hist_portb = 0xFF;     // default is high because the pull-up
+volatile Motor motor;
+
+volatile uint8_t overflows = 0;
+volatile uint8_t hist_portb = 0xFF;
 
 ISR(PCINT0_vect) {
 	uint8_t changed;
@@ -29,22 +49,50 @@ ISR(PCINT0_vect) {
 	changed = PINB ^ hist_portb;
 	hist_portb = PINB;
 
-	if(changed & (1 << PB0)) {
+	if(changed & (1 << PB0)) { // pb0 changed
+		// calculate speed
+		motor.inverse_speed = TCNT1 & 0x7FFF;
+		TCNT1 = 0;
+
 		if(PINB & (1 << PB0)) { // pb0 rising edge
-			if(PINB & (1 << PB1)) {
-				// forward
-				PORTD &= ~(1 << PIN_LED1);
-				PORTD |= 1 << PIN_LED0;
-			} else {
-				// backward
-				PORTD &= ~(1 << PIN_LED0);
-				PORTD |= 1 << PIN_LED1;
-			}
+			if(PINB & (1 << PB1)) // pb1 high or low?
+				motor.dir = LEFT;
+			else
+				motor.dir = RIGHT;
 		}
 	}
 }
 
+ISR(TIMER1_OVF_vect) {
+	overflows++;
+}
+
+void uart_init(unsigned int ubrr) {
+	// set tx output
+	PORTD |= 1 << PD1;
+
+	// set baud rate
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	
+	// enable rx and tx
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+	// set frame format: 8data, 2stop bit
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+void uart_tx(char c) {
+	// wait for empty tx buffer
+	while(!(UCSR0A & (1<<UDRE0))) asm volatile ("NOP");
+
+	// put char in buffer
+	UDR0 = c;
+}
+
 int main(void) {
+	uart_init(F_CPU/16/BAUD-1);
+
 	// setup pin change interrupts
 	PCICR = 1 << PCIE0;
 	PCMSK0 = 1 << PCINT0;
@@ -57,11 +105,20 @@ int main(void) {
 	DDRC |= (1 << 2);
 	PORTC |= (1 << 2);
 
-	//PORTD |= (1 << PIN_LED0) | (1 << PIN_LED1);
+	// setup 16 bit timer
+	TCCR1B = CLK_256;
+	TIFR1 = 1 << TOV1; // interrupt on overflow
 
 	sei();
 
 	forever {
+		uart_tx('h');
+		uart_tx('e');
+		uart_tx('l');
+		uart_tx('l');
+		uart_tx('o');
+		uart_tx('\n');
+
 		// forward
 		PORTC &= ~(1 << PIN_M_2A);
 		PORTC |= 1 << PIN_M_1A;
